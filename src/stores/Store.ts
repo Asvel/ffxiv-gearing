6db9cc860b6c1b80f3a6f95168ce9cdfb8a9957d
Asvel <fe.asvel@gmail.com>
2021-12-13 16:47:22 +0800

    Fix skeleton gears auto selection.
    (cherry picked from commit c8d9457b00cc4c7fc726c1fcc5318f9065caa4d7)

--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -389,7 +389,7 @@ export const Store = mst.types
         let lastMeldable = gears[gears.length - 1];
         if (lastMeldable === undefined || lastMeldable.isFood || lastMeldable.slot === 17) continue;
         for (let i = gears.length - 1; i >= 0; i--) {
-          if ((gears[i] as IGear).materias.length > 0) {
+          if ((gears[i] as IGear).materiaSlot > 0) {
             lastMeldable = gears[i];
             break;
           }
