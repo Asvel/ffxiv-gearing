25abde49c6659cb64c53a4160405d322d20ceceb
Asvel <fe.asvel@gmail.com>
2021-12-14 09:52:39 +0800

    Remove patch filter as this edition won't have new patch anymore.

--- src/views/FilterPanel.tsx
+++ src/views/FilterPanel.tsx
@@ -8,29 +8,6 @@ export const FilterPanel = mobxReact.observer(() => {
   return (
     <div className="filter card">
       <div className="filter_section">
-        <span className="filter_title">实装版本</span>
-        <span className="filter_sub">当前时间点国服和国际服的装备已经没有差别（直到国际服更新6.0）</span>
-      </div>
-      <div className="filter_controls">
-        <Radio
-          label={`显示全部`}
-          checked={store.filterPatch === 'all'}
-          onChange={() => store.setFilterPatch('all')}
-        />
-        <Radio
-          // label={`显示国服已实装和即将实装的项目（≤${G.patches.next}）`}
-          label={`显示国服已实装和即将实装的项目`}
-          checked={store.filterPatch === 'next'}
-          onChange={() => store.setFilterPatch('next')}
-        />
-        <Radio
-          // label={`只显示国服已实装的项目（≤${G.patches.current}）`}
-          label={`只显示国服已实装的项目`}
-          checked={store.filterPatch === 'current'}
-          onChange={() => store.setFilterPatch('current')}
-        />
-      </div>
-      <div className="filter_section">
         <span className="filter_title">“聚焦”模式</span>
         <span className="filter_sub">隐藏大部分装备，对比某几件</span>
       </div>
