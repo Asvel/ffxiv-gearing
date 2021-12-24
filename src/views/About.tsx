49395a856034d358d88e16a0a62f842825a0f554
Asvel <fe.asvel@gmail.com>
2021-12-24 14:49:47 +0800

    Treat gear with custom stats filled as melded when filter.
    (cherry picked from commit 12c102d)

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -5,7 +5,7 @@ export const About = mobxReact.observer(() => {
     <div className="about">
       <span className="about_name">最终幻想14配装器</span>
       <span className="about_name-suffix">怀旧版</span>
-      <span className="about_version">2112a</span>
+      <span className="about_version">2112b</span>
       <span className="about_separator">·</span>
       <a
         className="about_link"
@@ -30,4 +30,3 @@ export const About = mobxReact.observer(() => {
     </div>
   );
 });
-
