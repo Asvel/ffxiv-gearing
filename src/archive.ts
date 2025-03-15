44eacb9cad2b58c1b8c2e1c438f827f65869c1f5
Asvel <fe.asvel@gmail.com>
2025-03-15 22:46:53 +0800

    Progress the storage migration.

--- src/archive.ts
+++ src/archive.ts
@@ -41,28 +41,22 @@ export function load(key?: string): object | undefined {
     setCurrentKey(key);
   }
   if (key !== undefined) {
-    let archive = sessionStorage.getItem(key);
-    if (archive === null) {  // TODO: delete this old behavior compatible code
-      archive = localStorage.getItem(key);
-      if (archive !== null) {
-        sessionStorage.setItem(key, archive);
-      }
-    }
+    const archive = sessionStorage.getItem(key);
     return archive ? JSON.parse(archive) : undefined;
   }
 }
 
-// TODO: enable this cleaning after a period of time
-// if (localStorage.getItem('ffxiv-gearing.dt.archive-cleaned') === null) {
-//   const keys = Array.from({ length: localStorage.length }, (_, i) => localStorage.key(i)!);
-//   for (const key of keys) {
-//     if (key.startsWith('ffxiv-gearing')) {
-//       const suffix = key.slice('ffxiv-gearing'.length);
-//       if ((suffix[0] === '-' && (suffix.length === 9 || suffix.length === 15)) ||
-//         suffix.slice(3, 12) === '.archive.') {
-//         localStorage.removeItem(key);
-//       }
-//     }
-//   }
-//   localStorage.setItem('ffxiv-gearing.dt.archive-cleaned', 'true');
-// }
+// TODO: remove this cleaning when leave Dawntrail
+if (localStorage.getItem('ffxiv-gearing.archive-cleaned') === null) {
+  const keys = Array.from({ length: localStorage.length }, (_, i) => localStorage.key(i)!);
+  for (const key of keys) {
+    if (key.startsWith('ffxiv-gearing')) {
+      const suffix = key.slice('ffxiv-gearing'.length);
+      if ((suffix[0] === '-' && (suffix.length === 9 || suffix.length === 15)) ||
+        suffix.slice(3, 12) === '.archive.') {
+        localStorage.removeItem(key);
+      }
+    }
+  }
+  localStorage.setItem('ffxiv-gearing.archive-cleaned', 'true');
+}
